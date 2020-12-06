93cec7f5bfee21b4294b9f51a578443cd041897f
unknown <abdosalahnew2050@gmail.com>
2020-12-06 20:45:03 +0200

    make it better

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn ecomproject:app
+web: gunicorn ecomproject:wsgi
--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "ecomapp",
     'include_by_ajax',
     'django_ajax',
+    'gunicorn',
 ]
 
 MIDDLEWARE = [
